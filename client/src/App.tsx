7b4a5e2754a009f5169db8d40422ca34122ffb2a
Ethan <46520558+Dev-EthanR@users.noreply.github.com>
2026-07-22 13:12:04 +1000

    Setup the frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,12 @@
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-  return <div>Home</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App;
